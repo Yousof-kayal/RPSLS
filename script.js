8cba0f95857295a923bc0cf8426b85d5311dc3c0
yousof-kayal <y.kayal@hotmail.com>
2024-10-28 02:41:41 +0300

    refine computer choice function

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 /*
+A function that checks for the winner, taking in the computer and user choices
+*/
+
+
+/*
 Random Number Generator function that returns a number between 1 and 5 corresponsing a string of one of the choices:
 (Rock, Paper, Scissors, Lizard, Spock)
 */
 function getComputerChoice(min, max){
-const minceil = Math.ceil(min)
-const maxfloor = Math.floor(max)
+    const minceil = Math.ceil(min)
+    const maxfloor = Math.floor(max)
 
-const computerchoice = Math.floor(Math.random() * (maxfloor - minceil + 1) + minceil)
+    const computerchoice = Math.floor(Math.random() * (maxfloor - minceil + 1) + minceil)
 
-if (computerchoice == 1){
-    return ("Rock")
-}
-else if (computerchoice == 2){
-    return ("Paper")
-}
-else if (computerchoice == 3){
-    return ("Scissors")
-}
-else if (computerchoice == 4){
-    return ("Lizard")
-}
-else if (computerchoice == 5){
-    return ("Spock")
-}
+    const hand = ("Rock", "Paper", "Scissors", "Lizard", "Spock")
+
+    return hand[computerchoice]
+
 }
 
 
-console.log(getComputerChoice(1,5))
+
+console.log(getComputerChoice(0,4))
